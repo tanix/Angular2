29374bc3b36aa307dcec87bfb05ef4aa51e69ef3
Tatsiana_Tratsinnikava@epam.com <tatsiana_tratsinnikava@epbyminw1120.minsk.epam.com>
2017-03-22 17:10:09 +0300

    Try to add jquery and bootstrap like a vendor.

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -16,6 +16,9 @@ Error.stackTraceLimit = Infinity;
 require('core-js/es6');
 require('core-js/es7/reflect');
 
+require('jquery/dist/jquery');
+require('bootstrap/dist/js/bootstrap');
+
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/proxy'); // since zone.js 0.6.15
@@ -23,6 +26,7 @@ require('zone.js/dist/sync-test');
 require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
+
 
 // RxJS
 require('rxjs/Rx');
