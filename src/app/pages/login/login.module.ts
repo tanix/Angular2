7dbd3429812c2cd02370dba0e6881b4958148416
Tatsiana_Tratsinnikava@epam.com <tatsiana_tratsinnikava@epbyminw1120.minsk.epam.com>
2017-04-26 16:20:54 +0300

    Login module was renamed.

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // routes
-import { routes } from './home.routes';
+import { routes } from './login.routes';
 
 // custom components
 import { LoginComponent } from './login.component';
