e6938d0586fbbd31cb29bd2a19c5f8f2a828f026
Tatsiana_Tratsinnikava@epam.com <tatsiana_tratsinnikava@epbyminw1120.minsk.epam.com>
2017-05-18 20:34:54 +0300

    Guard was changed

--- src/app/core/services/routes-guard/can-activate-guard.ts
+++ src/app/core/services/routes-guard/can-activate-guard.ts
@@ -1,22 +1,18 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
-import { authorizationService } from '../authorization/authorization.service';
-import { Subscription } from 'rxjs/Subscription';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Injectable()
 export class canActivateGuard implements CanActivate, OnDestroy  {
 	private isLogined : boolean = false;
 
-	constructor(public authorizationService: authorizationService, public router: Router) {
+	constructor(public router: Router) {
 		console.log("canActivateGuard constructor");
 	}
 
 	canActivate(): boolean | Observable<boolean> {
 		console.log("canActivate method called");
-
-		console.log(localStorage.getItem("email"));
 
 		if(localStorage.getItem("email")) {
 			this.isLogined = true;
