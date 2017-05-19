f7e43c95dae2f75f19c9573839abfa3094ad38d4
Tatsiana_Tratsinnikava@epam.com <tatsiana_tratsinnikava@epbyminw1120.minsk.epam.com>
2017-05-19 16:37:02 +0300

    Authorization header was added

--- src/app/core/services/authenticated-http/authenticated-http.service.ts
+++ src/app/core/services/authenticated-http/authenticated-http.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SecureHttpService extends Http {
-	headers = new Headers({	'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': '123456789'});
+	headers = new Headers({	'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': localStorage.getItem("token")});
 
 	constructor(backend: ConnectionBackend, defaultOptions: RequestOptions) {
 		super(backend, defaultOptions);
@@ -22,9 +22,9 @@ export class SecureHttpService extends Http {
 		return super.get(url, options);
 	}
 
-	// post(url: string, options?: RequestOptionsArgs): Observable<Response> {
+	// post(url: string, options?: RequestOptionsArgs, body?:any): Observable<Response> {
 	// 	console.log('Extends Http post');
-	// 	options = new RequestOptions({ headers: this.headers, withCredentials: false });
+	// 	options = new RequestOptions({ headers: this.headers, withCredentials: false, body: body });
 	// 	return super.post(url, options);
 	// }
 }
